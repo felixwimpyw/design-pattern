1995d91a6fe7ceb690a148b021882f8419ae1ef1
Felix Wimpy Wijaya <felixwimpy@gmail.com>
2019-05-24 15:19:45 +0700

    Factory method model

---
+++ src/main/java/com/gdn/cc/designpattern/creational/factory/method/MazdaCar.java
@@ -0,0 +1,16 @@
+package com.gdn.cc.designpattern.creational.factory.method;
+
+public class MazdaCar implements Car {
+
+  @Override
+  public String printCar() {
+    return "Mazda";
+  }
+
+  @Override
+  public String runCar() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
